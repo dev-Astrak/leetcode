85455ad427788351286413b2114e55978d38c4c0
Astrak <vinitastrak04@gmail.com>
2026-08-28 21:02:55 +0530

    Time: 11 ms (90.91%), Space: 19.5 MB (68.18%) - LeetHub

--- 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.py
+++ 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.py
@@ -2,74 +2,66 @@ class Solution:
     def lexPalindromicPermutation(self, s: str, target: str) -> str:
         n = len(s)
         cnt = [0] * 26
+        for ch in s:
+            cnt[ord(ch) - 97] += 1
 
-        for c in s:
-            cnt[ord(c) - 97] += 1
+        odd_chars = [i for i in range(26) if cnt[i] % 2 == 1]
+        if n % 2 == 0:
+            if len(odd_chars) != 0:
+                return ""
+            mid_char = None
+        else:
+            if len(odd_chars) != 1:
+                return ""
+            mid_char = chr(odd_chars[0] + 97)
 
-        if sum(x & 1 for x in cnt) > 1:
-            return ""
-
-        half_cnt = [x // 2 for x in cnt]
         m = n // 2
+        pool = [cnt[i] // 2 for i in range(26)]
 
-        mid = ""
-        if n & 1:
-            for i in range(26):
-                if cnt[i] & 1:
-                    mid = chr(i + 97)
-                    break
-
-        def build(c):
-            return ''.join(chr(i + 97) * c[i] for i in range(26))
-
-        def make(left):
-            return left + mid + left[::-1]
-
-        prefix = target[:m]
-        used = [0] * 26
-        feasible = True
-
-        for c in prefix:
-            x = ord(c) - 97
-            used[x] += 1
-            if used[x] > half_cnt[x]:
-                feasible = False
+        # find longest feasible matched prefix length (max_i) for target[0:m] from pool
+        pool_states = [pool[:]]
+        cur = pool[:]
+        max_i = 0
+        for i in range(m):
+            c = ord(target[i]) - 97
+            if cur[c] > 0:
+                cur = cur[:]
+                cur[c] -= 1
+                pool_states.append(cur)
+                max_i = i + 1
+            else:
                 break
 
-        if feasible:
-            left = prefix
-            candidate = make(left)
-            if candidate > target:
-                return candidate
+        def build_sorted(p):
+            return ''.join(chr(i + 97) * p[i] for i in range(26))
 
-        for i in range(m - 1, -1, -1):
-            used = [0] * 26
-            ok = True
+        # Candidate: full prefix match (longest possible), check if resulting palindrome beats target
+        if max_i == m:
+            Q = target[:m]
+            P = Q + (mid_char if mid_char else "")
+            full = P + Q[::-1]
+            if full > target:
+                return full
 
-            for j in range(i):
-                x = ord(target[j]) - 97
-                used[x] += 1
-                if used[x] > half_cnt[x]:
-                    ok = False
+        limit = max_i if max_i < m else m - 1
+
+        for i in range(limit, -1, -1):
+            remaining = pool_states[i]
+            tc = ord(target[i]) - 97
+            chosen = -1
+            for c in range(tc + 1, 26):
+                if remaining[c] > 0:
+                    chosen = c
                     break
-
-            if not ok:
+            if chosen == -1:
                 continue
 
-            cur = ord(target[i]) - 97
-
-            for x in range(cur + 1, 26):
-                if used[x] < half_cnt[x]:
-                    used[x] += 1
-                    remain = [
-                        half_cnt[j] - used[j]
-                        for j in range(26)
-                    ]
-
-                    left = target[:i] + chr(x + 97) + build(remain)
-                    candidate = make(left)
-
-                    if candidate > target:
-                        return candidate
+            newpool = remaining[:]
+            newpool[chosen] -= 1
+            fill = build_sorted(newpool)
+            Q = target[:i] + chr(chosen + 97) + fill
+            P = Q + (mid_char if mid_char else "")
+            full = P + Q[::-1]
+            return full
 
         return ""
