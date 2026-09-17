161977db812421604ea0aa924818a1366da9f212
Astrak <vinitastrak04@gmail.com>
2026-09-17 21:53:47 +0530

    Time: 100 ms (87.71%), Space: 31 MB (83.24%) - LeetHub

---
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minSumOfLengths(self, arr: list[int], target: int) -> int:
+        n = len(arr)
+        best = [float('inf')] * n
+        left = 0
+        curr = 0
+        ans = float('inf')
+        min_len = float('inf')
+
+        for right in range(n):
+            curr += arr[right]
+
+            while curr > target:
+                curr -= arr[left]
+                left += 1
+
+            if curr == target:
+                length = right - left + 1
+
+                if left > 0 and best[left - 1] != float('inf'):
+                    ans = min(ans, length + best[left - 1])
+
+                min_len = min(min_len, length)
+
+            best[right] = min_len
+
+        return -1 if ans == float('inf') else ans
